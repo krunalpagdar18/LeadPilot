89ba528cdc2414ee4e1041d4a75ed3bc9d346f83
Krunal pagdar <krunalpagdar@gmail.com>
2026-03-06 18:15:29 +0530

    Migration changes for Lead deletion and updation

---
+++ LeadPilot/Migrations/20260306113528_LeadUpdate_DeleteChanges.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeadPilot.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeadUpdate_DeleteChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Inactive",
+                table: "Lead",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InactiveDate",
+                table: "Lead",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inactive",
+                table: "Lead");
+
+            migrationBuilder.DropColumn(
+                name: "InactiveDate",
+                table: "Lead");
+        }
+    }
+}
